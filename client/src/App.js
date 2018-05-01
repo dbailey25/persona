bc308c849fc3fc522e7c6d85c9b3f090b83510bb
dbailey25 <dtbailey25@gmail.com>
2018-05-01 17:27:32 -0400

    remove MVP

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./pages/Login";
 import Host from "./pages/Host";
-// import Waiter from "./pages/Waiter";
-// import Customer from "./pages/Customer";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 import { Container } from "./components/Grid";
 import Waiter from "./pages/Waiter";
 import Footer from "./components/Footer";
-import WebcamCapture from "./App0"
 
 let userName = "N/A";
 
@@ -20,11 +17,11 @@ const App = () => (
       <Container>
       <h4>User: {userName}</h4>
       <Switch>
-<Route exact path="/app0" component={WebcamCapture} />
         <Route exact path="/" component={Login} />
         <Route exact path="/host" component={Host} />
         <Route exact path="/waiter" component={Waiter} />
         {/*<Route exact path="/customer" component={Customer} />*/}
+        <Route exact path="/*" component={Login} />
         <Route component={NoMatch} />
       </Switch>
       </Container>
