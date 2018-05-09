c56afb5a5d1ac67aeb80ac1575b13fdcb8c10074
varan2030 <varan2030@>
2018-05-09 14:59:50 -0400

    add app

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongoose.connect(
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("./client/build"));
 }
 
 // // Send every request to the React app
