ab8aaeb951cb0bd5988eb01fe845fd995324746f
varan2030 <varan2030@>
2018-05-09 15:13:29 -0400

    add app

--- server.js
+++ server.js
@@ -27,13 +27,13 @@ mongoose.connect(
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build/static"));
+  app.use(express.static("/client/build/"));
 }
 
 // // Send every request to the React app
 // // Define any API routes before this runs
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
 app.listen(PORT, function() {
