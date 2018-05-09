c5aa7b87c9784cfa4018009ad1686b23805b7485
varan2030 <varan2030@>
2018-05-09 19:32:15 -0400

    heroku

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ app.use(bodyParser.text({ type: 'text/html' }))
 
 // app.use(express.static('public'));
 
-//app.use(express.static("./client/build"));
-app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static("client/build"));
+//app.use(express.static(path.join(__dirname, '/client/build/index.html')));
 
 
 app.use(routes);
@@ -39,9 +39,9 @@ mongoose.connect(
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", function(req, res) {
+//  // res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
