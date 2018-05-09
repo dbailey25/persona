e868d1ff5644be9ba3fcb4830f26a4b8e107d6bb
varan2030 <varan2030@>
2018-05-09 15:20:30 -0400

    add app

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:500
 
 // app.use(express.static('public'));
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use(routes);
 
