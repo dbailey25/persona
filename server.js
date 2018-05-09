105562512af81b7277fdfe5906b05029b05a5b5d
varan2030 <varan2030@>
2018-05-09 14:43:01 -0400

    add app

--- server.js
+++ server.js
@@ -30,11 +30,11 @@ mongoose.connect(
 //   app.use(express.static("client/build"));
 // }
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// // Send every request to the React app
+// // Define any API routes before this runs
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
