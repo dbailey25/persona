42b68cd881270aa6bf48d30088fcde52a5f8495a
varan2030 <varan2030@>
2018-05-09 15:09:26 -0400

    add app

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const mongoose = require("mongoose");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Serve up static assets
-app.use(express.static("client/public"));
+app.use(express.static("client/build"));
 // Add routes, both API and view
 app.use(routes);
 
