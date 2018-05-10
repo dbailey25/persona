99d69a22e12d939223c09ff27995ac0433af31fd
varan2030 <varan2030@>
2018-05-09 20:46:44 -0400

    heroku

--- server.js
+++ server.js
@@ -33,15 +33,15 @@ mongoose.connect(
 
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 
 // Send every request to the React app
 // Define any API routes before this runs
-// app.get("*", function(req, res) {
-//  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+app.get("/", function(req, res) {
+ res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
