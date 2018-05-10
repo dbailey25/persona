97fdd59116d24e273710aafdca7912e32530bd25
varan2030 <varan2030@>
2018-05-09 20:03:01 -0400

    heroku

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.text({ type: 'text/html' }))
 
 // app.use(express.static('public'));
 
-app.use(express.static("client/build"));
+// app.use(express.static("client/build"));
 //app.use(express.static(path.join(__dirname, '/client/build/index.html')));
 
 
