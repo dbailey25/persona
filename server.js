7faffa18193b9ea0c82a623676079248b4c1d80f
varan2030 <varan2030@>
2018-05-17 12:23:20 -0400

    add app

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.text({ type: 'text/html' }))
 // app.use(express.static('public'));
 
 app.use(express.static("client/build"));
-//app.use(express.static(path.join(__dirname, '/client/build/index.html')));
+app.use(express.static(path.join(__dirname, '/client/build/index.html')));
 
 
 app.use(routes);
