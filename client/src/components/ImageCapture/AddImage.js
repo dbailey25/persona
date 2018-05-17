109b7d9e3e6f8e1c033c127f463e08e23f3f31b3
dbailey25 <dtbailey25@gmail.com>
2018-05-17 11:39:25 -0400

    clear add, full fix

--- client/src/components/ImageCapture/AddImage.js
+++ client/src/components/ImageCapture/AddImage.js
@@ -3,13 +3,14 @@ import "./ImageCapture.css"
 
 export const AddImage = props => (
   <form className={props.visibility}>
-  <div className="text-center">
+  <div className="text-center font">
+    <div className="top-margin-15">Enter Name</div>
       <input
         className="form-control form-input-pers"
         value={props.name}
         onChange={props.handleInputChange}
         name="name"
-        placeholder="Name"
+        placeholder={props.name}
       />
        <button type="submit" className="btn button" onClick={props.addPhoto}>Add Photo to Collection</button>
        <div id='addConfirm'>{props.addConfirm}</div>
